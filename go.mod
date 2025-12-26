14e9691cd29907aa16585d735363c35f904b89b6
define42 <define42@github.com>
2025-12-26 18:41:22 +0100

    Moved staticAuth into own file

--- go.mod
+++ go.mod
@@ -6,16 +6,13 @@ toolchain go1.24.11
 
 require (
 	github.com/bolkedebruin/rdpgw v1.0.4
-	github.com/go-ldap/ldap/v3 v3.4.12
+	golang.org/x/crypto v0.36.0
 )
 
 require (
-	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/go-asn1-ber/asn1-ber v1.5.8-0.20250403174932-29230038a667 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
@@ -23,7 +20,6 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.10.0 // indirect
 	github.com/prometheus/procfs v0.1.3 // indirect
-	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	google.golang.org/protobuf v1.23.0 // indirect
 )
---
+++ static_auth.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+/*
+   ---------------------------
+   Static Authenticator
+   ---------------------------
+*/
+
+type StaticAuth struct {
+	mu         sync.Mutex
+	challenges map[string]ntlmChallengeState
+}
+
+const staticUser = "ubuntu"
+const staticPassword = "ubuntu"
+
+type authChallenge struct {
+	header string
+}
+
+func (a authChallenge) Error() string {
+	return "authentication challenge"
+}
+
+func (a *StaticAuth) Authenticate(
+	ctx context.Context,
+	r *http.Request,
+) (string, error) {
+
+	fmt.Println(r)
+	authHeader := strings.TrimSpace(r.Header.Get("Authorization"))
+	if authHeader != "" {
+		scheme, token := splitAuthHeader(authHeader)
+		if scheme != "" && (strings.EqualFold(scheme, "NTLM") || strings.EqualFold(scheme, "Negotiate")) {
+			canonicalScheme := canonicalAuthScheme(scheme)
+			if token == "" {
+				return "", authChallenge{header: canonicalScheme}
+			}
+			decoded, err := base64.StdEncoding.DecodeString(token)
+			if err != nil {
+				log.Printf("%s token decode failed from %s: %v", canonicalScheme, r.RemoteAddr, err)
+				return "", authChallenge{header: canonicalScheme}
+			}
+			ntlmToken := decoded
+			if canonicalScheme == "Negotiate" {
+				ntlmToken, err = extractNTLMToken(decoded)
+				if err != nil {
+					log.Printf("Negotiate token missing NTLM for %s: %v", r.RemoteAddr, err)
+					return "", authChallenge{header: canonicalScheme}
+				}
+			}
+			msgType, err := ntlmMessageType(ntlmToken)
+			if err != nil {
+				log.Printf("Invalid NTLM message from %s: %v", r.RemoteAddr, err)
+				return "", authChallenge{header: canonicalScheme}
+			}
+			switch msgType {
+			case ntlmMessageTypeNegotiate:
+				return "", a.ntlmChallengeError(r, canonicalScheme)
+			case ntlmMessageTypeAuthenticate:
+				user, err := a.verifyNTLMAuthenticate(r, ntlmToken, canonicalScheme, staticUser, staticPassword)
+				if err != nil {
+					return "", err
+				}
+				return normalizeUser(user), nil
+			default:
+				return "", authChallenge{header: canonicalScheme}
+			}
+		}
+	}
+
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		return "", errors.New("missing credentials")
+	}
+	if username != staticUser || password != staticPassword {
+		return "", errors.New("invalid username or password")
+	}
+
+	return normalizeUser(username), nil
+}
+
+func splitAuthHeader(header string) (string, string) {
+	header = strings.TrimSpace(header)
+	if header == "" {
+		return "", ""
+	}
+	for i, r := range header {
+		if r == ' ' || r == '\t' {
+			return header[:i], strings.TrimSpace(header[i+1:])
+		}
+	}
+	return header, ""
+}
+
+func canonicalAuthScheme(scheme string) string {
+	if strings.EqualFold(scheme, "Negotiate") {
+		return "Negotiate"
+	}
+	return "NTLM"
+}
