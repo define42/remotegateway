8afb1fad779af9ac52e0fa097dcf0ef4abf69170
define42 <define42@github.com>
2025-12-30 23:08:12 +0100

    Password in session it now ntlm-hash

--- ntlm.go
+++ ntlm.go
@@ -8,7 +8,6 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -134,11 +133,12 @@ func (a *StaticAuth) verifyNTLMAuthenticate(r *http.Request, data []byte, scheme
 		log.Printf("NTLM auth failed, user %q not found", msg.UserName)
 		return "", a.ntlmChallengeError(r, scheme)
 	}
-	fmt.Println("userLdap.User.Password", userLdap.User.Password)
+	//fmt.Println("userLdap.User.Password", userLdap.User.Password)
 
-	ntlmV2Hash := ntlmV2Hash(userLdap.User.Password, msg.UserName, msg.DomainName)
+	//ntlmV2Hash := ntlmV2Hash(userLdap.User.Password, msg.UserName, msg.DomainName)
+	//ntlmV2Hash := userLdap.User.NtlmPassword
 
-	if !verifyNTLMv2Response(challenge, ntlmV2Hash, msg.NtChallengeResponse) {
+	if !verifyNTLMv2Response(challenge, userLdap.User.NtlmPassword, msg.NtChallengeResponse) {
 		log.Printf("NTLM auth failed for user=%q domain=%q", msg.UserName, msg.DomainName)
 		return "", a.ntlmChallengeError(r, scheme)
 	}
